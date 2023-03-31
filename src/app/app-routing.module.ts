8be642c679ecb8de14c496c1c8b815089d09c8e4
Sivanesh2 <88311943+Sivanesh2@users.noreply.github.com>
2023-04-01 00:39:48 +0530

    routing issue fix 1 testing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path:'',redirectTo:'MCA',pathMatch:'full'},
   {path:'MCA',loadChildren:()=>import('./Modules/mca/mca.module').then((m)=>m.MCAModule),},
+  {path:'**',loadChildren:()=>import('./Modules/mca/mca.module').then((m)=>m.MCAModule),}
   
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
